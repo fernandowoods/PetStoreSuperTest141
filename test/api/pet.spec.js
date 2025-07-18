5f9b367c977817ae9f6a1ec53a78cbae39c2bddc
fernandowoods <fewoods16@gmail.com>
2025-07-18 19:01:30 -0300

    Adicionado metodo POST com massa

--- test/api/pet.spec.js
+++ test/api/pet.spec.js
@@ -8,6 +8,7 @@ describe('API PetStore Swagger - Pet', () =>{
 
     // Atributos do grupo/describe
     const request = supertest('https://petstore.swagger.io/v2') // BaseURL
+    const massa1 = require('../../vendors/json/massaPet')
 
     // Funções ou Métodos: Its
     it('POST Pet', async () => { 
@@ -26,6 +27,32 @@ describe('API PetStore Swagger - Pet', () =>{
                 expect(res.body.tags[0].name).toBe('vaccinated') // o [0] representa que verá a primeira tag listada (a contagem começa em zero)
             }) 
     }) // Final do método POST
+
+    it.each(massa1.array.map(elemento => [
+        elemento.name,
+        elemento.idPet,
+        elemento.categoryId,
+        elemento.categoryName
+    ]))
+    ('POST Pet Data Driven Simples: %s', async (name, idPet, categoryId, categoryName) => { // %s para trazer o primeiro campo da massa de dados
+        const pet = require('../../vendors/json/pet.json')
+        // Substituimos os campos que queremos personalizar através da massa
+        pet.id = idPet
+        pet.name = name
+        pet.category.id = categoryId
+        pet.category.name = categoryName
+
+          return request
+            .post('/pet')
+            .send(pet)
+            .then((res) => {  // res = response abreviado
+                expect(res.statusCode).toBe(200)
+                expect(res.body.id).toBe(idPet)
+                expect(res.body.name).toBe(name)
+                expect(res.body.category.name).toBe(categoryName)
+                expect(res.body.tags[0].name).toBe('vaccinated') // o [0] representa que verá a primeira tag listada (a contagem começa em zero)
+            })
+    })
 
     it('GET Pet', async () => {
         return await request
---
+++ vendors/json/massaPet.js
@@ -0,0 +1,20 @@
+module.exports.array = [
+  {
+    "name": "Snoopy",
+    "idPet": 312217301,
+    "categoryId": 1,
+    "categoryName": "dog"
+  },
+  {
+    "name": "Doug",
+    "idPet": 312217302,
+    "categoryId": 1,
+    "categoryName": "dog"
+  },
+  {
+    "name": "Spike",
+    "idPet": 312217303,
+    "categoryId": 2,
+    "categoryName": "cat"
+  }
+]
